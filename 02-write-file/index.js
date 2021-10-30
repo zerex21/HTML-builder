fcae766ca687d3cc65204d1dcc82588c6e18c172
oleg <zerex21@icloud.com>
2021-10-30 15:00:09 +0300

    feat: add task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const readline = require('readline');
+const process = require ('process');
+
+
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+});
+
+
+rl.question('Enter text ... ', (answer) => {
+  fs.writeFile('02-write-file/text.txt',answer,(err)=>{
+    if(err) return err;
+  })
+ 
+});
+
+
+rl.on('line', text=>{
+  fs.appendFile('02-write-file/text.txt',text,{flags:'a'},(err)=>{
+      if(err) throw err;
+      if(text === 'exit'){
+        console.log('Bye!')
+        process.exit()
+      }
+      console.log(`Your typed: ${text} `);
+    });
+
+});
+
+process.openStdin().on("keypress", function(chunk, key) {
+  if(key && key.name === "c" && key.ctrl) {
+    console.log("Bye Bye!");
+    process.exit();
+  }
+});
