0c7a3152c2931428091864802309ce87388d16ec
oleg <zerex21@icloud.com>
2022-05-16 17:17:08 +0300

    feat: task 2 prettier

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const readline = require('readline');
-const process = require ('process');
+const process = require('process');
 
 
 const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 fs.open('02-write-file/text.txt', 'w', (err) => {
-  if(err) throw err;
+  if (err) throw err;
 });
 
-process.openStdin().on('keypress', function(chunk, key) {
-  if(key && key.name === 'c' && key.ctrl) {
+process.openStdin().on('keypress', function (chunk, key) {
+  if (key && key.name === 'c' && key.ctrl) {
     console.log('Bye Bye!');
     process.exit();
   }
@@ -21,29 +21,30 @@ process.openStdin().on('keypress', function(chunk, key) {
 
 
 rl.question('Enter text ... ', (answer) => {
-  if(answer=='exit'){
+  if (answer == 'exit') {
     console.log('Bye!')
     process.exit();
   }
-  fs.writeFile('02-write-file/text.txt',answer,(err)=>{
-    if(err) return err;
+  fs.writeFile('02-write-file/text.txt', answer, (err) => {
+    if (err) return err;
   });
 });
 
 
-rl.on('line', text=>{
-  if(text === 'exit'){
-    text=''
-    console.log('Bye!'); 
+rl.on('line', text => {
+  if (text === 'exit') {
+    text = ''
+    console.log('Bye!');
     process.exit();
-     
+
   }
-  fs.appendFile('02-write-file/text.txt',text,{flags:'a'},(err)=>{
-    if(err) throw err;
-    if(text === 'exit'){
-      console.log('Bye!');    
+  fs.appendFile('02-write-file/text.txt', text, {
+    flags: 'a'
+  }, (err) => {
+    if (err) throw err;
+    if (text === 'exit') {
+      console.log('Bye!');
     }
     console.log(`Your typed: ${text} `);
   });
 });
-
